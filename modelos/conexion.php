fc9b5597532f1d0246ecc0114aabaa686e6c415e
Vectorez Dev <info@vectorez.com>
2022-03-09 13:20:43 -0500

    12
    12

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -5,7 +5,7 @@
 		static public function conectar()
 		{	
 			//conexion PDO
-			$link = new PDO("mysql:host=142.93.126.221;dbname=map_place", "sylar_admin", "Sylar_Admin*2018");
+			$link = new PDO("mysql:host=1412122.91213.121226.22112;dbname=map_place12", "sylar_admin121", "1212");
 			//seteamos los caracteres latinos
 			$link->exec("set names utf8");
 			return $link;
